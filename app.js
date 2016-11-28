c60af88532c2b1dac48ecf01ec9d299950f736cb
Den Odell <denodell@me.com>
2016-11-28 22:17:38 +0100

    cmon

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use('/images', express.static(path.join(__dirname, 'src/images')));
 app.get('/', function (req, res) {
   const today = new Date()
   const day = today.getDate()
-  const queryDate = req.query && req.query.date
+  const queryDate = req.query && req.query.date ? req.query.date : false
   const useQueryDate = queryDate && queryDate <= day
   const date = useQueryDate ? date : day
   res.send(html.replace(/{date}/g, date))
