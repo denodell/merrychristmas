da4ad22dd18d6c64ec81eda47293bae117eb8a25
Den Odell <denodell@me.com>
2016-11-28 22:04:06 +0100

    images

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var compression = require('compression'),
     path = require('path'),
+    fs = require('fs'),
     express = require('express'),
     app = express()
     port = process.env.PORT;
 
-app.use('/', express.static(path.join(__dirname, 'src/')));
+app.use('/images', express.static(path.join(__dirname, 'src/images')));
+app.get('/', function (req, res) {
+  res.send(fs.readFileSync(path.join(__dirname, '/src/index.html'), 'utf-8'))
+})
 
 app.use(compression());
 app.listen(port, function() {
