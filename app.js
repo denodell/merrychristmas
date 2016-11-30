2b7736969d54f2afac5ceb5ddfdbeb18117905ef
Den Odell <den.odell@akqa.com>
2016-11-30 17:51:10 +0100

    port fix

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var compression = require('compression'),
     fs = require('fs'),
     express = require('express'),
     app = express(),
-		isProd = process.env.production && process.env.production === 'true',
-    port = isProd ? process.env.PORT : 5005,
+    port = process.env.PORT,
     html = fs.readFileSync(path.join(__dirname, '/src/index.html'), 'utf-8');
 
 app.use('/images', express.static(path.join(__dirname, 'src/images')));
